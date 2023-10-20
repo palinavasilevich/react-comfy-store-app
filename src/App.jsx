1370bb4bde119a8709ba64ec7e3a56bff5022e9a
PolinaVasilevich <polina.vasilevichh@gmail.com>
2023-10-20 13:30:14 +0200

    feat: add SingleProduct page

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,8 @@ import {
 import { ErrorElement } from "./components";
 
 //loaders
-import { loader as landingLoader, loader } from "./pages/Landing";
+import { loader as landingLoader } from "./pages/Landing";
+import { loader as singleProductLoader } from "./pages/SingleProduct";
 
 const router = createBrowserRouter([
   {
@@ -38,6 +39,8 @@ const router = createBrowserRouter([
       {
         path: "products/:id",
         element: <SingleProduct />,
+        errorElement: <ErrorElement />,
+        loader: singleProductLoader,
       },
       {
         path: "cart",
