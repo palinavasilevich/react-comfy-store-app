fb10dc456c35109b5b4c50fdc91a83a1754526b5
PolinaVasilevich <polina.vasilevichh@gmail.com>
2023-10-25 17:55:19 +0200

    feat: add filters

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,8 +1,12 @@
 import React from "react";
-import { Link, Form } from "react-router-dom";
+import { Link, Form, useLoaderData } from "react-router-dom";
 import FormInput from "./FormInput";
+import FormSelect from "./FormSelect";
 
 const Filters = () => {
+  const { meta } = useLoaderData();
+  const { categories, companies } = meta;
+
   return (
     <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
       {/* SEARCH */}
@@ -12,6 +16,31 @@ const Filters = () => {
         name="search"
         size="input-sm"
       />
+
+      {/* CATEGORIES */}
+      <FormSelect
+        label="select categories"
+        name="categories"
+        list={categories}
+        size="select-sm"
+      />
+
+      {/* COMPANIES */}
+      <FormSelect
+        label="select company"
+        name="company"
+        list={companies}
+        size="select-sm"
+      />
+
+      {/* ORDER */}
+      <FormSelect
+        label="sort by"
+        name="order"
+        list={["a-z", "z-a", "high", "low"]}
+        size="select-sm"
+      />
+
       {/* BUTTONS */}
       <button type="submit" className="btn btn-primary btn-sm">
         search
