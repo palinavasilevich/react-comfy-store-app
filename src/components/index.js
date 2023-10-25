6ae591fc3a4dcc8a8d6d4d95d46a5ef83ba6f671
PolinaVasilevich <polina.vasilevichh@gmail.com>
2023-10-25 17:52:16 +0200

    feat: add FormSelector component

--- src/components/index.js
+++ src/components/index.js
@@ -12,3 +12,4 @@ export { default as Filters } from "./Filters";
 export { default as ProductsContainer } from "./ProductsContainer";
 export { default as PaginationContainer } from "./PaginationContainer";
 export { default as ProductsList } from "./ProductsList";
+export { default as FormSelect } from "./FormSelect";
