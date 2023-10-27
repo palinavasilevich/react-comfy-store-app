589cae687b402294362a9380e63407508d95ca72
PolinaVasilevich <polina.vasilevichh@gmail.com>
2023-10-27 13:51:39 +0200

    feat: setup RTK and react-toastify

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ToastContainer } from "react-toastify";
+import { store } from "./store";
+import { Provider } from "react-redux";
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <>
+  <Provider store={store}>
     <App />
-  </>
+    <ToastContainer position="top-center" />
+  </Provider>
 );
