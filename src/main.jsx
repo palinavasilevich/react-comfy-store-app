68f6a1479976e42f811c14bced9ba9aba8bd93ba
PolinaVasilevich <polina.vasilevichh@gmail.com>
2023-10-27 14:11:44 +0200

    fix: fix error with react-toastify

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "react-toastify/dist/ReactToastify.css";
+
+import "./index.css";
+
 import { ToastContainer } from "react-toastify";
 import { store } from "./store";
 import { Provider } from "react-redux";
-import App from "./App.jsx";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
